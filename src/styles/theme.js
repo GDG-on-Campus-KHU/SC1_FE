ab9ef9b7d38e1bc4bf2eb8c8ae1cf964d57a66de
youngju6143 <youngju6143@naver.com>
2024-11-19 13:22:18 +0900

    first commit

---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    primary: {
+      100: "#c21d03",
+      200: "#fd5732",
+      300: "#ffb787",
+    },
+    accent: {
+      100: "#393939",
+      200: "#bebebe",
+    },
+    text: {
+      100: "#232121",
+      200: "#4b4848",
+    },
+    background: {
+      100: "#fbfbfb",
+      200: "#f1f1f1",
+      300: "#c8c8c8",
+    },
+  },
+};
+
+export default theme;
