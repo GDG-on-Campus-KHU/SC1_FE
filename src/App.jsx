82090e73d81de804be41a36ce6228bbb6a771f4b
영주 <111039206+youngju6143@users.noreply.github.com>
2024-11-26 14:05:31 +0900

    📦️ Chore: GlobalStyle 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import GlobalStyles from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
