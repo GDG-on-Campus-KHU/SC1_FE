6b35a4796d557073985dcafd7ea43995fdb3d479
youngju6143 <youngju6143@naver.com>
2024-11-28 23:37:19 +0900

    ✨ Feat: 메인 - header, footer 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,16 +1,18 @@
+import Footer from "../../components/footer";
+import Header from "../../components/header/Header";
 import HashtagHeader from "./components/HashtagHeader";
 import NewsSection from "./components/NewsSection";
 import * as S from "./Mainpage.style";
 
 export default function Main() {
-
   return (
     <S.App>
+      <Header page="뉴스" />
       <S.Container>
         <HashtagHeader />
         <NewsSection />
       </S.Container>
+      <Footer isNews={true} />
     </S.App>
-
   );
 }
