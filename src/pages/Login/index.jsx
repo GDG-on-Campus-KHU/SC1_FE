3ae7374d53f823a5bb17ae534191ebce9a0f5dee
youngju6143 <youngju6143@naver.com>
2024-11-29 17:55:25 +0900

    ✨ Feat: 구글 로그인 함수 구현

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,28 @@
 import * as S from "./Login.style";
 import logo from "../../assets/logo.svg";
+import { useGoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const G_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID;
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = useGoogleLogin({
+    clientId: G_CLIENT_ID,
+    onSuccess: (res) => {
+      console.log(res);
+
+      localStorage.setItem("accessToken", res.access_token);
+      localStorage.setItem("userName", res.data.user_info.name);
+      localStorage.setItem("userId", res.data.user_info.id);
+
+      navigate("/");
+      // 구글 로그인 성공 시 처리할 로직
+    },
+    onError: (err) => {
+      console.log(err);
+      // 구글 로그인 실패 시 처리할 로직
+    },
+  });
   return (
     <S.App>
       <S.Container>
@@ -8,7 +30,7 @@ export default function Login() {
         <p>중요한 순간, 정확한 정보로 안전을 지켜드려요.</p>
         <img src={logo} alt="logo" />
         <h1>재난노티</h1>
-        <S.LoginButton></S.LoginButton>
+        <S.LoginButton onClick={handleGoogleLogin}></S.LoginButton>
       </S.Container>
     </S.App>
   );
