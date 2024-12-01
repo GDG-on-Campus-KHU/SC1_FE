12d575b470bc1228e8d204cef73fd49251dc9926
youngju6143 <youngju6143@naver.com>
2024-12-01 19:48:01 +0900

    🐛 Fix: GoogleOAuthProvider 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,13 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
-const G_CLIENT_ID = import.meta.env.VITE_APP_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={G_CLIENT_ID}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
